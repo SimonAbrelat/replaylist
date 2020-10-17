d9397e9a59e9a54b18422c27d11e37ad8922aba6
Taleb Hirani <thirani7@gatech.edu>
2020-10-17 14:53:15 -0400

    Updated redirects.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,12 +1,13 @@
 (() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams);
     if (!urlParams.has('access_token')) {
-        //location.assign("https://simonabrelat.github.io/replaylist")
+        location.assign("https://simonabrelat.github.io/replaylist")
     }
     const tok = urlParams.get('access_token');
     const tok_type = urlParams.get('token_type');
     const expires = urlParams.get('expires_in');
     console.log(tok);
-    //location.assign("https://simonabrelat.github.io/replaylist")
+    location.assign("https://simonabrelat.github.io/replaylist/recorder.html")
 })()
