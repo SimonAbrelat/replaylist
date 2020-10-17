2627b11ea949723a7f859375293c9742b14edcaf
Taleb Hirani <thirani7@gatech.edu>
2020-10-17 18:33:17 -0400

    FIXED: URL Parsing

--- js/redirect.js
+++ js/redirect.js
@@ -3,17 +3,11 @@
     arr = queryString.split("#")[1].split("&");
     if (arr.length < 3) {
         alert("Login Failed.")
-    }
-    const urlParams = new URLSearchParams(queryString);
-    console.log(urlParams);
-    if (!urlParams.has('access_token')) {
-        //alert("Login failed.")
-        //location.assign("https://simonabrelat.github.io/replaylist")
-    }
+        location.assign("https://simonabrelat.github.io/replaylist")
 
+    }
     sessionStorage.setItem("tok", arr[0].split("=")[1]);
     sessionStorage.setItem("tok_type", arr[1].split("=")[1]);
     sessionStorage.setItem("expires", arr[2].split("=")[1]);
-
-    //location.assign("https://simonabrelat.github.io/replaylist/recorder.html")
+    location.assign("https://simonabrelat.github.io/replaylist/recorder.html")
 })()
