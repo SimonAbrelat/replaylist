2a229851ba18d7f635e0b921a090f6aeca2e3f66
Taleb Hirani <thirani7@gatech.edu>
2020-10-17 16:35:16 -0400

    refactors requests, removes spotify calls before tok

--- js/API.js
+++ js/API.js
@@ -1,39 +1,32 @@
-const HttpClient = function(contentType = null) {
-    this.get = function(aUrl, key) {
-        return new Promise((resolve, reject) => {
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.open("GET", aUrl, true);
-            anHttpRequest.setRequestHeader("Authorization", key);
-            anHttpRequest.setRequestHeader("Content-Type", contentType);
-            anHttpRequest.send(null);
-            anHttpRequest.onload = () => {
-                resolve(anHttpRequest.response);
-            }
-            anHttpRequest.onerror = () => {
-                reject(ananHttpRequest.response);
-                console.log("API call to Spotify failed");
-            }
-        });
-    }
-}
-
 const Spotify = function(key) {
     this.key = key;
     this.client = new HttpClient();
     this.user_id;
-    this.client.get('https://api.spotify.com/v1/me', this.key).then(res => {
-        this.user_id = res.id;
-    })
+    
+    this.get = function(url) {
+        return fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + key
+            }
+        })
+    }
+
+    this.get('https://api.spotify.com/v1/me').then(res => console.log(res));
     
     this.getRecentlyPlayed = function() {
-        this.client.get('https://api.spotify.com/v1/me/player/recently-played', this.key).then(res => {
+        this.get('https://api.spotify.com/v1/me/player/recently-played')
+        .then(res => {
             console.log(res);
         })
     }
 
     this.getPlaylists = function() {
-        this.client.get('https://api.spotify.com/v1/users/' + user_id + '/playlists', this.key).then(res => {
-            console.log(res);
+        this.get('https://api.spotify.com/v1/users/' + user_id + '/playlists')
+        .then(res => {
+                console.log(res);
         })
     }
 
@@ -47,4 +40,4 @@ const Spotify = function(key) {
     }
 }
 
-var spotify = new Spotify(null);
+var spotify = null;
