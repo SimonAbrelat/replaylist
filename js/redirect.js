48871def7b220fed9a1b74148ac5c12e4be1845d
Taleb Hirani <thirani7@gatech.edu>
2020-10-17 16:57:31 -0400

    Typo fix

--- js/redirect.js
+++ js/redirect.js
@@ -4,7 +4,7 @@
     console.log(urlParams);
     if (!urlParams.has('access_token')) {
         alert("Login failed.")
-        location.assign("https://simonabrelat.github.io/replaylist")
+        //location.assign("https://simonabrelat.github.io/replaylist")
     }
     const tok = urlParams.get('access_token');
     const tok_type = urlParams.get('token_type');
