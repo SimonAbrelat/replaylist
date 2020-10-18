b6ccff88efea4fd4a1953a769fa8e86619a12aaa
Taleb Hirani <thirani7@gatech.edu>
2020-10-17 21:34:05 -0400

    Added capability to get playlist

--- js/API.js
+++ js/API.js
@@ -2,6 +2,7 @@ class Spotify {
     constructor(key) {
         this.key = key;
         this.user_id = null;
+        getMe();
     }
     
     get(url) {
@@ -18,21 +19,34 @@ class Spotify {
     }
 
     getMe() {
-        this.get('https://api.spotify.com/v1/me').then(r => this.user_id = r.id);
-        console.log(this.user_id);
+        this.get('https://api.spotify.com/v1/me')
+        .then(r => this.user_id = r.id)
     }
 
     getRecentlyPlayed() {
         this.get('https://api.spotify.com/v1/me/player/recently-played')
-        .then(r => {
-            console.log(r);
+        .then(function(result) {
+            return r.items;
         })
     }
 
     getPlaylists() {
         this.get('https://api.spotify.com/v1/users/' + this.user_id + '/playlists')
-        .then(r => {
-                console.log(r);
+        .then(function(result) {
+            return r.items;
+        })
+    }
+
+    addToPlaylist(playlistID, uriList) {
+        var url = "https://api.spotify.com/v1/playlists/" + playlistID + "/tracks";
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.key
+            },
+            body: uriList
         })
     }
 
@@ -43,5 +57,15 @@ class Spotify {
         parameters.collaborative = isCollaborative;
         parameters.description = description;
         var jsonParameters = JSON.stringify(parameters);
+        var url = "https://api.spotify.com/v1/users/" + this.user_id + "/playlists"
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.key
+            },
+            body: jsonParameters
+        })
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
-function parseDescription(descStr) {
-    tags = descStr.split("#",3)
+const s = new Spotify(sessionStorage.getItem("tok"));
+var activePL = null;
+
+
+function getRePlaylists() {
+    let totalList = s.getPlaylists();
+    let ourList = [];
+    for (let i = 0; i < pList.length; i++) {
+        tags = totalList[i].description.split("#",2)
+        if (tags[0] == "replaylist") {
+            ourList.push(totalList[i])
+        }
+    }
+    return ourList;
 }
+
+function setPlaylists(playlists) {
+    let options = document.getElementById("playlists");
+    playlists.forEach((x) => {
+        let opt = document.createElement("option");
+        opt.text = x.name;
+        options.add(opt, options[0]);
+    });
+}
+/*
+function createHistoryPL() {
+    playlistID = s.createPlaylist("replaylist", false, false, "#replaylist#");
+    hist = s.getRecentlyPlayed();
+    addList = []
+    for (track in hist) {
+        addList.push(track.track.uri);
+    }
+    s.addToPlayList(list, addList)
+}
+*/
